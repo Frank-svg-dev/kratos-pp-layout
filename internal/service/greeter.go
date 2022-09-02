1348d05e0c62435f003df2b04e76ac576cbb6703
Frank_Peng <2061801790@qq.com>
2022-09-02 16:48:01 +0800

    fix:初始化环境:kratos-pp-layout

---
+++ internal/service/greeter.go
@@ -0,0 +1,31 @@
+package service
+
+import (
+	"context"
+	"kratos-pp-layout/internal/data"
+
+	v1 "kratos-pp-layout/api/helloworld/v1"
+	"kratos-pp-layout/internal/biz"
+)
+
+// GreeterService is a greeter service.
+type GreeterService struct {
+	v1.UnimplementedGreeterServer
+
+	uc *biz.GreeterUsecase
+}
+
+// NewGreeterService new a greeter service.
+func NewGreeterService(uc *biz.GreeterUsecase) *GreeterService {
+	return &GreeterService{uc: uc}
+}
+
+// SayHello implements helloworld.GreeterServer.
+func (s *GreeterService) SayHello(ctx context.Context, in *v1.HelloRequest) (*v1.HelloReply, error) {
+	g, err := s.uc.CreateGreeter(ctx, &biz.Greeter{Hello: in.Name})
+	if err != nil {
+		return nil, err
+	}
+
+	return &v1.HelloReply{Message: "Hello " + g.Hello}, nil
+}
